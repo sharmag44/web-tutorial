9d0e2933fa95aba6f0d200d1fef57b2bb728a6de
sharmag44 <sharmagshambhu44@gmail.com>
2021-11-28 11:08:34 +0530

    links pages created

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,13 +1,11 @@
 import Footer from "./Footer/Footer";
 import Page1 from "./Page1/Page1";
-import ES6 from "./ES6/ES6";
 import "../styles.css";
 
 export default function Main() {
   return (
     <div classsName="Main">
       <Page1 />
-      <ES6 />
       <Footer />
     </div>
   );
