a6f31810edc1e9c448e8d4d4bc5e972d054bc67b
sharmag44 <sharmagshambhu44@gmail.com>
2021-12-09 18:11:46 +0530

    usestate create

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Header from "./Components/Header/Header";
 import Top from "./Components/Top/Top";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 export default function App() {
   return (
-    <div className="App">
+    <div className="Ccontainer-fluid App">
       <Top />
       <Header />
       <Main />
--- src/Components/Section/Hooks/Hooks.js
+++ src/Components/Section/Hooks/Hooks.js
@@ -12,7 +12,49 @@ function Hooks() {
         Hooks to reuse stateful behavior between different components.
       </p>
 
-      <Code data="" />
+      <h4>Types of Hooks in React</h4>
+      <p>useState Hook</p>
+      <p>useEffect Hook</p>
+      <p>useRef Hook</p>
+      <p>useCallback Hook</p>
+      <p>useMemo Hook</p>
+      <p>useContext Hook</p>
+      <p>useReducer Hook</p>
+
+      <h5>useState Hook</h5>
+      <p>The useState hook allows us to create state variables in a React function component.</p>
+      <p> State allows us to access and update certain values in our components over time</p>
+
+      <div className="Code__all">
+        <Code data="import { useState } from 'react';" />
+        <Code data="import ReactDOM from ' react-dom';" />
+        <Code data="function FavoriteColor() {" />
+        <Code data="const [color, setColor] = useState(' red');" />
+        <Code data="  return <h1>My favorite color is {color}!</h1>" />
+        <Code data="}" />
+        <Code data="" />
+        <Code data="ReactDOM.render(<FavoriteColor />, document.getElementById('root'));" />
+      </div>
+
+      <h5>Update State</h5>
+      <p>To update our state, we use our state updater function.</p>
+      <div className="Code__all">
+        <Code data="import { useState } from 'react';" />
+        <Code data="import ReactDOM from ' react-dom';" />
+        <Code data="function FavoriteColor() {" />
+        <Code data="const [color, setColor] = useState(' red');" />
+        <Code data=" return (" />
+        <Code data="<>" />
+        <Code data=" <h1>My favorite color is {color}!</h1>" />
+        <Code data="<button
+        type='button'
+        onClick={() => setColor('blue')}
+      >Blue</button>" />
+        <Code data="</>" />
+        <Code data="}" />
+        <Code data="" />
+        <Code data="ReactDOM.render(<FavoriteColor />, document.getElementById('root'));" />
+      </div>
     </div>
   );
 }
